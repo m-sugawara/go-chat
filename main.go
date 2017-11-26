cdf6b8267956690b7ce2d4ab8b68ec3a11584663
m-sugawara <masa.sugawar@gmail.com>
2017-11-26 19:51:19 +0900

    add: addr argument

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"sync"
 	"text/template"
 	"path/filepath"
+	"flag"
 )
 
 type templateHandler struct {
@@ -18,17 +19,21 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "Appliation address")
+	flag.Parse()
+
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	// Start chat room
 	go r.run()
 	// Launch web server
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Launch web server....port: ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 
